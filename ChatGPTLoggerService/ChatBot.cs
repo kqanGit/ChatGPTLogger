7ed56d25e8e7e9baaa95b16f046310d9e7d1ba74
NhanVo <nhanthvo810909@gmail.com>
2025-10-09 16:52:28 +0700

    send request + recieve response to ollama

---
+++ ChatGPTLoggerService/ChatBot.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+using PowerShellHandler;
+
+namespace ChatGPTLoggerService
+{
+    public static class ChatBot
+    {
+        /// <summary>
+        /// Chạy chatbot trên console. Nhập close để kết thúc.
+        /// </summary>
+        static void Start()
+        {
+            string message = string.Empty;
+            do
+            {
+                message = Console.ReadLine()!;
+                if (message == string.Empty) continue;
+                if (message == "close") break;
+                string response = Chat(message);
+                Console.WriteLine(response);
+            } while (message != "close");
+        }
+
+        /// <summary>
+        /// Gửi request đến ollama
+        /// </summary>
+        /// <param name="message">Prompt được sử dụng</param>
+        /// <returns>Kết quả response</returns>
+        static string Chat(string message)
+        {
+            //Thay bằng địa chỉ ngrok được tạo
+            string address = "https://unaggravated-benedictory-paige.ngrok-free.dev";
+
+            string cmd = $"(Invoke-WebRequest -method POST -Body " +
+                $"'{{\"model\":\"gpt-oss:20b\", \"prompt\":\"{message}\", \"stream\": false}}' " +
+                $"-uri {address}/api/generate).Content | ConvertFrom-json";
+
+            //Chạy lệnh powershell gửi request
+            string chatResult = PowerShellHandler.PowerShellHandler.ExecCommand(cmd);
+
+            //Lấy phần response
+            int responseStart = chatResult.IndexOf("response             :") + ("response             :".Length);
+            int responseEnd = chatResult.IndexOf("thinking             :");
+            string chatResponse = chatResult.Substring(responseStart, (responseEnd - responseStart));
+
+            //Loại bỏ space thừa 
+            chatResponse = chatResponse.Replace("\r", string.Empty);
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[ ]{2,}", options);
+            chatResponse = regex.Replace(chatResponse, " ");
+
+            return chatResponse;
+        }
+    }
+}
---
+++ ChatGPTLoggerService/PSHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Management.Automation;
+using System.Text;
+
+namespace PowerShellHandler;
+
+/// <summary>
+/// Xử lý powershell
+/// </summary>
+public static class PowerShellHandler
+{
+    private static PowerShell ps = PowerShell.Create();
+
+    /// <summary>
+    /// Chạy command trong powershell
+    /// </summary>
+    /// <param name="command">Lệnh được gọi</param>
+    /// <returns>Kết quả thực thi lệnh ở dạng chuỗi</returns>
+    public static string ExecCommand(string command)
+    {
+        string errorMsg = string.Empty;
+
+        ps.AddScript(command);
+        ps.AddCommand("Out-string");
+
+        PSDataCollection<PSObject> outputCollection = new();
+        ps.Streams.Error.DataAdded += (object sender, DataAddedEventArgs e) =>
+        {
+            errorMsg = ((PSDataCollection<ErrorRecord>)sender)[e.Index].ToString();
+        };
+
+        IAsyncResult result = ps.BeginInvoke<PSObject, PSObject>(null, outputCollection);
+
+        ps.EndInvoke(result);
+
+        StringBuilder sb = new();
+
+        foreach (var outputItem in outputCollection)
+        {
+            sb.AppendLine(outputItem.BaseObject.ToString());
+        }
+
+        ps.Commands.Clear();
+
+        if (!string.IsNullOrEmpty(errorMsg))
+            return errorMsg;
+
+        return sb.ToString().Trim();
+    }
+}
