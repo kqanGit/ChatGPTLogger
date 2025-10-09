02aae569d5ca36c800627999cccdb4e0badcb629
Jinn-645 <luchoangtuan2005@gmail.com>
2025-10-09 15:17:48 +0700

    Complete txtLogger

--- TxtLogger/Class1.cs
+++ TxtLogger/Class1.cs
@@ -1,7 +1,31 @@
-﻿namespace TxtLogger
-{
-    public class Class1
-    {
+﻿using ILogging;
 
+namespace TxtLogger
+{
+    public class TxtLogger : ILogger
+    {
+        public void Log(string request, string response)
+        {
+            var now = DateTime.Now;
+            var filename = $"log{now.Year}{now.Month}{now.Day}.txt";
+
+            string line = $"{now:yy:MM:dd HH:mm:ss} | Request: {request} | Response: {response}";
+
+            if (!File.Exists(filename))
+            {
+                using (var writer = new StreamWriter(filename, append: false))
+                {
+                    writer.WriteLine(line);
+                }
+            }
+            else
+            {
+                using (var writer = new StreamWriter(filename, append: true))
+                {
+                    writer.WriteLine(line);
+                }
+            }
+            return ;
+        }
     }
 }
